bc821a5052aa4b2b6a8b3cb2b730316bcdf53578
Vansh Dalal <142863429+Vanshdalal314@users.noreply.github.com>
2025-05-15 10:44:10 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose';
-     const uri = ``;
-     
-
+     const uri = `mongodb+srv://dalalvvvvansh:KcEDkQruTuo8ya7C@cluster012.7pza9ev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster012`;
      export const connectDB = async () => {
        try {
          await mongoose.connect(uri);

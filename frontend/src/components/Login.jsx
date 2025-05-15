9de00afd8760777d98b4ab19906c0d3119f2d745
Vansh Dalal <142863429+Vanshdalal314@users.noreply.github.com>
2025-05-15 11:02:00 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = ({ onSubmit, onSwitchMode }) => {
   const [rememberMe, setRememberMe] = useState(false)
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
-  const url = "http://localhost:4000"
+  const url = "https://task-manager-backend-tll4.onrender.com"
 
   // Auto-login
   useEffect(() => {
